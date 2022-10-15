4f370962fbcbd581d2c6616525d0e5b2bc838b00
harold caicedo <haroldo_89@hotmail.com>
2022-10-15 13:20:21 +0200

    implementacion de introduccion, sintesis y actividad

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -1,23 +1,23 @@
 <template lang="pug">
-  .curso-main-container.pb-3
-    BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
-    .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-  
-      .titulo-sexto.color-acento-contenido.mb-2.pr-2
-        h5 Gestión de Analítica de Datos
-        br
-        em Síntesis: fundamentos de programación 
-      
-      p.pt-4.pb-5 En el siguiente mapa podrá apreciar los principales elementos de la arquitectura de datos:
-  
-  
-      .row.justify-content-center
-        .col-lg-10.mb-5
-          figure
-            img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
-      
-  
-  </template>
+.curso-main-container.pb-3
+  BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    .titulo-sexto.color-acento-contenido.mb-2.pr-2
+      h5 Gestión de Analítica de Datos
+      br
+      em Síntesis: fundamentos de programación 
+    
+    p.pt-4.pb-5 En el siguiente mapa podrá apreciar los principales elementos de la arquitectura de datos:
+
+
+    .row.justify-content-center
+      .col-lg-10.mb-5
+        figure
+          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+    
+
+</template>
 
 <script>
 export default {
