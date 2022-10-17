f8d2da3d0d160191b62e34b78d35a6072551b0e0
harold caicedo <haroldo_89@hotmail.com>
2022-10-17 21:19:11 +0200

    se agrega router a tema4 y tema5

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -7,6 +7,17 @@
         span 4
       h1 Titulo de primer nivel
 
+    .row.justify-content-center
+      .col-10
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-5 
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-8
+              .bloque-texto-a__texto.p-4
+                p.mb-0 El modelado de datos es un proceso que permite analizar y definir los datos generados por la organización y la relación entre estos, con el fin de identificar sus características y dependencias. Un modelo de datos bien definido permite crear sistemas de información eficientes con estructuras de datos simplificadas mejorando así el rendimiento del sistema y por consiguiente los resultados que arroja.
+            .col-lg-4.mb-4.mb-lg-0(data-aos="fade-left")
+              figure
+                img(src="@/assets/curso/temas/tema2/modelo_datos.png").img100
+
 </template>
 
 <script>
