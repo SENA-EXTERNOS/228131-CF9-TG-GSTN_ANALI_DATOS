4d33cb40c98b61fe3c3b2598656b0382ebf9fe6f
harold.caicedo <harold.caicedo>
2022-11-01 13:34:30 +0100

    integracion de actividad didactica

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .curso-main-container.pb-3
-  BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
+  BannerInterno(icono="fas fa-tasks" titulo="Fundamentos de modelamiento de datos")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
@@ -20,7 +20,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Arrastrar / soltar
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
                 
